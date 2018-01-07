ac1cf08d70162f0bbaea7788c8394f20f2cc7f45
ThiagoRAD <thauroo@gmail.com>
2018-01-06 22:42:09 -0300

    refactor

--- version2.js
+++ version2.js
@@ -16,18 +16,15 @@ async function doCommits(commit = 'update') {
     const differenceInDays = Math.floor((today - date) / (1000 * 60 * 60 * 24));
     const message = `git commit --date "${differenceInDays} day ago" -m "${commit}"`;
     
-    // Get list of modified files
     const {stdout} = await execAsync('git ls-files --others --modified --deleted --exclude-standard');
     const files = stdout.split('\n').filter((f) => f);
     
-    // Add and commit each file
     for (const file of files) {
       await execAsync(`git add ${file}`);
       await execAsync(message, {env: {...process.env, GIT_COMMITER_DATE: date.toISOString()}});
       console.log(date.toISOString());
     }
     
-    // Push changes
     await execAsync('git push');
     console.log('All commits completed successfully');
     
