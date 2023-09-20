03dea96213b86df71be6fdda53c15dbc38d40596
ThiagoRAD <thauroo@gmail.com>
2023-09-19 22:40:23 -0300

    update

--- version2.js
+++ version2.js
@@ -1,5 +1,7 @@
 const process = require('process');
 const {exec} = require('child_process');
+const util = require('util');
+const execAsync = util.promisify(exec);
 
 getRandomDate = () => {
   const start = new Date(2018, 0, 1);
@@ -7,29 +9,30 @@ getRandomDate = () => {
   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 };
 
-function doCommits(commit = 'update') {
-  const date = getRandomDate();
-  const today = new Date();
-  const differenceInDays = Math.floor((today - date) / (1000 * 60 * 60 * 24));
-  const message = `git commit --date "${differenceInDays} day ago" -m "${commit}"`;
-  exec('git ls-files --others --modified --deleted --exclude-standard', (error, stdout, stderr) => {
+async function doCommits(commit = 'update') {
+  try {
+    const date = getRandomDate();
+    const today = new Date();
+    const differenceInDays = Math.floor((today - date) / (1000 * 60 * 60 * 24));
+    const message = `git commit --date "${differenceInDays} day ago" -m "${commit}"`;
+    
+    // Get list of modified files
+    const {stdout} = await execAsync('git ls-files --others --modified --deleted --exclude-standard');
     const files = stdout.split('\n').filter((f) => f);
+    
+    // Add and commit each file
     for (const file of files) {
-      exec(`git add ${file}`, () => {
-        exec(message, {env: {...process.env, GIT_COMMITER_DATE: date.toISOString()}}, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error executing command: ${error.message}`);
-            return;
-          }
-          if (stderr) {
-            console.error(`Error: ${stderr}`);
-            return;
-          }
-          exec('git push');
-        });
-      });
+      await execAsync(`git add ${file}`);
+      await execAsync(message, {env: {...process.env, GIT_COMMITER_DATE: date.toISOString()}});
     }
-  });
+    
+    // Push changes
+    await execAsync('git push');
+    console.log('All commits completed successfully');
+    
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 }
 
 const commitMessageRegex = /version2\.js\s(.+)/;
